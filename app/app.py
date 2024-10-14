cb08e7b1c18ce26911b2e6888694d990316a2aa5
Ammar-theScorpion <assiammar692@gmail.com>
2024-10-15 00:36:33 +0300

    add project section

--- app/app.py
+++ app/app.py
@@ -4,9 +4,9 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/static", StaticFiles(directory="./static"), name="static")
 
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory="./templates")
 
 
 @app.get("/{username}", response_class=HTMLResponse)
